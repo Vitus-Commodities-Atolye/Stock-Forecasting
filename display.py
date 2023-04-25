384f8a9fb8935e9ddb34adffd62cea0efa07f15e
Burak Keçeci <bburakkececi@gmail.com>
2023-04-25 23:32:54 +0300

    forecast & actual line graph is added

--- display.py
+++ display.py
@@ -1,3 +1,5 @@
+import datetime
+
 import pandas as pd
 from dash import Dash, dcc, html, Input, Output
 import plotly.express as px
@@ -5,83 +7,89 @@ import plotly.graph_objects as go
 
 # reading the csv files and convert them to data frame
 stocks = pd.read_csv("historical_results.csv")
+stocks['Date'] = pd.to_datetime(stocks['Date'])
+
+tickers = stocks['Stock'].sort_values().unique()
+min_date = stocks['Date'].min()
+max_date = (stocks['Date'].max() - pd.DateOffset(days=30)).date()
+
 forecasts = pd.read_csv("prediction_results.csv")
+forecasts['Date'] = pd.to_datetime(forecasts['Date'])
 
 # init Dash app
 app = Dash(__name__)
+app.title = "Stock Market Insider"
 
 # layout of the Dash app
 app.layout = html.Div([
-    html.Div(
-        [
-            html.Div(
-                html.H1("Stock Forecast App"),
-                className="header"
-            ),
-
-            html.Div([
+    html.Header([
+        html.H1("Stock Market"),
+        html.H1("Insider")
+    ], className="header"),
+    html.Main([
+        html.Div(
+            [
                 html.Div(
                     [
                         html.P("Select stock:"),
-                        dcc.Dropdown(options=stocks["Stock"].unique(), value="Ticker Name", id="stock_code")
-                    ], className="inputs"
+                        dcc.Dropdown(options=tickers, value=None, id="stock_code")
+                    ], className="input"
                 ),
-
                 html.Div(
                     [
-                        html.P("Select stock you want to compare:"),
-                        dcc.Dropdown(options=stocks["Stock"].unique(), value=None, id="stock_code_cmp")
-                    ], className="inputs"
+                        html.P("Compare stock:"),
+                        dcc.Dropdown(options=tickers, value=None, id="stock_code_cmp")
+                    ], className="input"
                 ),
-
+                html.Div(
+                    [
+                        html.P("Select interval:"),
+                        # Date range input, default value will be defined from data
+                        dcc.DatePickerRange(
+                            id='date-range',
+                            min_date_allowed=min_date,
+                            max_date_allowed=max_date,
+                            start_date=min_date,
+                            end_date=max_date
+                        )
+                    ], className="date"
+                ),
                 html.Div(
                     [
                         html.P("Forecast range in days:"),
                         dcc.Dropdown(options=[7, 14, 30], value=7,
                                      id="forecast_day")
-                    ], className="inputs"
+                    ], className="input"
                 ),
 
-                html.Div(
-                    [
-                        html.P("Select start date:"),
-                        # Date range input, default value will be defined from data
-                        dcc.DatePickerSingle(
-                            id='date-range',
-                            min_date_allowed=stocks['Date'].min(),
-                            max_date_allowed=stocks['Date'].max(),
-                            date=stocks['Date'].min()
-                        )
-                    ], className="inputs"
-                ),
             ], className="input-box"),
 
+        html.Div([
             html.Div(
-                html.P("Copyright © 2023"),
-                className="copyright"
+                [
+                    dcc.Loading(children=[html.Div([], id='stonks-graph', className='graphs')], id='loading1',
+                                type='graph'),
+                    dcc.Loading(id='loading2',
+                                children=[html.Div([], id='forecast-graph', className='graphs')],
+                                type='graph')
+                ], className="graphs"
             )
-        ], className='nav'),
 
-    html.Div([
-        html.Div(
-            [
-                dcc.Loading(children=[html.Div([], id='stonks-graph', className='graphs')], id='loading1',
-                            type='graph'),
-                dcc.Loading(id='loading2',
-                            children=[html.Div([], id='forecast-graph', className='graphs')],
-                            type='graph')
-            ]
+        ], className='outputContainer'
         )
-
-    ], className='outputContainer'
-    )
-], className='container'
-)
-
+    ], className='container'
+    ),
+    html.Footer([
+        html.Div(
+            html.P("Copyright © 2023"),
+            className="copyright"
+        )
+    ])
+])
 
 """
-Precondition : Ticker name and start date should be defined. To compare with another ticker needs a ticker name 
-should be provided (optional). 
+Precondition : Ticker name and start date should be defined. To compare with another ticker needs a ticker name
+should be provided (optional).
 
 Post-condition : The stock trend of the ticker with given date is provided.
 
@@ -90,112 +98,53 @@ Post-condition : The stock trend of the ticker with given date is provided.
 @:param date_range : start date of the ticker's stock trend
 @:return stonks-graph : graphs of the stock trend
 """
+
+
 @app.callback(
     Output('stonks-graph', 'children'),
     Input('stock_code', 'value'),
     Input('stock_code_cmp', 'value'),
-    Input('date-range', 'date')
-)
-def update_stock_graph(stock_code, stock_code_cmp, date):
-    # query for gathering related ticker data.
-    df1 = stocks.query(f"Stock in ['{stock_code}']")
-
-    # define start and end dates for date range
-    start_date = date
-    end_date = df1['Date'].max()
-
-    # draws a line of the given ticker's data
-    fig = px.line(
-        df1,
-        x='Date',
-        y='Close',
-        range_x=[start_date, end_date],
-        color=df1['Stock'],
-        labels={'Close': 'Price'}
-    )
-
-    if stock_code_cmp is not None:
-        # query for gathering related ticker data.
-        df2 = stocks.query(f"Stock in ['{stock_code_cmp}']")
-
-        end_date = df2['Date'].max()
-
-        fig.add_trace(px.line(
-            df2,
-            x='Date',
-            y='Close',
-            range_x=[start_date, end_date],
-            color=df2['Stock'],
-            color_discrete_sequence=["red"]
-        ).data[0], )
-
-    fig.update_layout(
-        title=dict(text="Stock Trend", font=dict(size=30))
-    )
-
-    return dcc.Graph(figure=fig)
-
-
-"""
-Precondition : Ticker name and interval should be defined. To compare with another ticker needs a ticker name 
-should be provided (optional). 
-
-Post-condition : The forecast trend of the ticker with given interval is provided.
-
-@:param stock_code : ticker name
-@:param stock_code_cmp (optional) : ticker name for comparing the stock trend of the given ticker
-@:param date_range : interval of the forecast in days
-@:return stonks-graph : graphs of the forecasted ticker's trend
-"""
-@app.callback(
-    Output('forecast-graph', 'children'),
-    Input('stock_code', 'value'),
-    Input('stock_code_cmp', 'value'),
+    Input('date-range', 'start_date'),
+    Input('date-range', 'end_date'),
     Input('forecast_day', 'value')
 )
-def update_forecast_graph(stock_code, stock_code_cmp, interval):
-    # query for gathering related ticker data.
-    df1 = forecasts.query(f"Stock in ['{stock_code}']")
-
-    # convert 'Date' column to datetime format
-    forecasts['Date'] = pd.to_datetime(forecasts['Date'])
-
-    # define start and end dates for date range
-    start_date = forecasts['Date'].min().date()
-    end_date = (start_date + pd.DateOffset(days=int(interval))).date()
-
+def update_stock_graph(stock_code, stock_code_cmp, start_date, end_date, interval):
     # Create the figure and add two scatter traces
     fig = go.Figure()
 
-    fig.add_trace(go.Scatter(x=df1['Date'],
-                             y=df1['Actual'],
-                             name=f'{stock_code} Actual'))
+    # query for gathering related ticker data.
+    stock = stocks.query(f"Stock in ['{stock_code}']")
+    if not stock.empty:
+        p_end_date = (pd.Timestamp(end_date) + pd.DateOffset(days=int(interval))).date()
 
-    fig.add_trace(go.Scatter(x=df1['Date'],
-                             y=df1['Prediction'],
-                             name=f'{stock_code} Prediction',
-                             line=dict(dash='dash')))
+        stock_interval = stock.loc[
+            (stock['Date'] >= pd.Timestamp(start_date)) & (stock['Date'] <= pd.Timestamp(end_date))]
+        # draws a line of the given ticker's data
+        fig.add_trace(go.Scatter(
+            x=stock_interval['Date'],
+            y=stock['Close'],
+            mode='lines',
+            name=f'{stock_code}',
+        ))
 
-    if stock_code_cmp is not None:
-        # query for gathering related ticker data.
-        df2 = forecasts.query(f"Stock in ['{stock_code_cmp}']")
+        forecast = forecasts.query(f"Stock in ['{stock_code}']")
+        forecast_interval = forecast.loc[
+            (forecast['Date'] >= (pd.Timestamp(end_date) - pd.DateOffset(days=2))) & (forecast['Date'] <= pd.Timestamp(p_end_date))]
+        print(forecast_interval)
+        fig.add_trace(go.Scatter(
+            x=forecast_interval['Date'],
+            y=forecast['Prediction'],
+            mode='lines',
+            name=f'{stock_code} Prediction',
+            line=dict(dash='dash'),
+        ))
 
-        # add two scatter traces
-        fig.add_trace(go.Scatter(x=df2['Date'],
-                                 y=df2['Actual'],
-                                 name=f'{stock_code_cmp} Actual'))
-
-        fig.add_trace(go.Scatter(x=df2['Date'],
-                                 y=df2['Prediction'],
-                                 name=f'{stock_code_cmp} Prediction',
-                                 line=dict(dash='dash')))
-
-    fig.update_layout(
-        title=dict(text="Forecast Trend", font=dict(size=30)),
-        xaxis_title='Date',
-        yaxis_title='Price'
-    )
-    fig.update_xaxes(range=[start_date, end_date])
+        fig.add_trace(go.Scatter(
+            x=forecast_interval['Date'],
+            y=forecast_interval['Actual'],
+            mode='lines',
+            name=f'{stock_code} Actual',
+        ))
 
     return dcc.Graph(figure=fig)
 
